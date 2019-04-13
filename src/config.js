42d051ee144456f6653bc7f4406ef5ca1ca30c53
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2019-04-13 02:56:59 -0700

    fixed API_BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+window.location.href.indexOf("localhost") > -1
+  ? "http://localhost:8080/api"
+  : "https://hidden-springs-67954.herokuapp.com/api";
